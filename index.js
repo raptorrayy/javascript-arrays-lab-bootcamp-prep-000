8985ca83975d3579616ee13ba44948dbf6b22fc1
Raymond W. Robinson <raymondrobinson123@gmail.com>
2018-01-20 21:38:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(){
+  const newKittens = [...kittens, "Broom"]
+  return newKittens
+}
+
+function prependKitten(){
+  const newKittens = ["Arnold", ...kittens]
+  return newKittens
+}
+
